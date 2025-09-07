da7c0e376147df4f5cfad19d52a8cb5c9e59d3e2
Samuel16299 <samuelparulian2005@gmail.com>
2025-09-07 23:05:50 +0800

    Update biner.dart

--- week2/biner.dart
+++ week2/biner.dart
@@ -1,1 +1,17 @@
+import 'dart:io';
 
+String toBinary(int n) {
+  return n.toRadixString(2);
+}
+
+void main() {
+  stdout.write("Masukkan angka N (>=0): ");
+  int n = int.parse(stdin.readLineSync()!);
+
+  if (n < 0) {
+    print("Input harus >= 0");
+    return;
+  }
+
+  print("Bilangan $n dalam biner adalah: ${toBinary(n)}");
+}
